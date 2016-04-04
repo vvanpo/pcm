4a39c13aeb273bbd59c4110501457aa0fa2105a3
Victor van Poppelen <vvanpo@gmail.com>
2016-04-04 06:11:49 -0700

    Removed uart polling

--- uvcs/test.c
+++ uvcs/test.c
@@ -7,8 +7,17 @@
 #define BAUD 250000
 #include <util/setbaud.h>
 
-int main (void) {
-    // Initialize UART
+void init_uart ();
+
+int main (void)
+{
+    init_uart();
+    
+    return 0;
+}
+
+void init_uart (void)
+{
     UBRR0H = UBRRH_VALUE;
     UBRR0L = UBRRL_VALUE;
 #if USE_2X
@@ -17,13 +26,4 @@ int main (void) {
     UCSR0A &= ~_BV(U2X0);
 #endif
     UCSR0B = _BV(TXEN0) | _BV(RXEN0);
-    char c;
-    while (1) {
-        c = 0;
-        while (!(UCSR0A & _BV(RXC0)));
-        c = UDR0;
-        while (!(UCSR0A & _BV(UDRE0)));
-        UDR0 = c;
-    }
-    return 0;
 }
